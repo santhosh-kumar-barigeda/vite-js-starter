ba36f97a2860d8262a5152ebf0daf8c97fc9ba9f
Santhosh <santhosh10378@gmail.com>
2025-03-09 07:54:22 +0530

    Added Redux

--- src/providers/index.jsx
+++ src/providers/index.jsx
@@ -1,3 +1,6 @@
+import { Provider } from 'react-redux';
+import store from '../store/store';
+
 export default function Providers({ children }) {
-  return <div>{children}</div>;
+  return <Provider store={store}>{children}</Provider>;
 }
